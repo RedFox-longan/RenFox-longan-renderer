53f5be78d41ecaf57e46b01d9f930717bf98889b
beanflame <beanflame@qq.com>
2021-12-05 16:15:05 +0800

    Create main.c

---
+++ demo/source/main.c
@@ -0,0 +1,12 @@
+//
+// Created by beanflame - 2021/11/10
+//
+
+#include <iostream>
+#include "space_longan_renderer.h"
+using namespace std;
+
+int main() {
+    cout << "Hello, world!" << endl;
+    return 0;
+}
